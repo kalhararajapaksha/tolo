234892fa8a8854976eca532f9723b4c35a2ef19f
Bhagya1024 <bhagya.amarasinghe1024@gmail.com>
2021-03-15 11:27:01 +0530

    I made some changes in the sign up and sign in page

--- app/src/main/java/com/example/tolo1/MainActivity.java
+++ app/src/main/java/com/example/tolo1/MainActivity.java
@@ -8,24 +8,41 @@ import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
     private androidx.appcompat.widget.AppCompatButton signin;
+    private Button signupbtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         signin=(androidx.appcompat.widget.AppCompatButton) findViewById(R.id.signin);
+        signupbtn=(Button)findViewById(R.id.signupbtn);
+
         signin.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View v)
+            public void onClick(View signin)
             {
                 opensignin();
             }
         });
+
+
+    signupbtn.setOnClickListener(new View.OnClickListener(){
+        @Override
+        public void onClick(View signup)
+        {
+            opensignup();
+        }
+    });
     }
     public void opensignin(){
         Intent intent = new Intent(this,signin.class);
         this.startActivity(intent);
     }
+    public void opensignup(){
+        Intent intent = new Intent(this,signup.class);
+        this.startActivity(intent);
+    }
+
 
 
 }
