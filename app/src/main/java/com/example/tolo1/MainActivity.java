b91d6d4a95913dfa2ece1d4d09e49b089523e5cc
dtkiriella <dtkiriella@users.noreply.github.com>
2021-03-20 13:37:34 +0530

    SignUp Validation

--- app/src/main/java/com/example/tolo1/MainActivity.java
+++ app/src/main/java/com/example/tolo1/MainActivity.java
@@ -5,27 +5,35 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.content.Intent;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     private androidx.appcompat.widget.AppCompatButton signin;
+    private Button signupbtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         signin=(androidx.appcompat.widget.AppCompatButton) findViewById(R.id.signin);
+        signupbtn =(Button) findViewById(R.id.signupbtn);
         signin.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v)
             {
-                opensignin();
+                Intent i = new Intent(MainActivity.this,signin.class);
+                startActivity(i);
+                finish();
             }
         });
-    }
-    public void opensignin(){
-        Intent intent = new Intent(this,signin.class);
-        this.startActivity(intent);
-    }
+        signupbtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v)
+            {
+                Intent i = new Intent(MainActivity.this,signup.class);
+                startActivity(i);
+            }
+        });
 
 
 }
